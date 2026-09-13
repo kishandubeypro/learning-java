aad0720b333931b374ea55f13f35acb2be759b12
Kishan Dubey <ishandubey0001@gmail.com>
2026-09-13 23:40:43 +0530

    made a sum program

---
+++ day01/sum.java
@@ -0,0 +1,21 @@
+package day01;
+
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first no. ");
+        int a=sc.nextInt();
+        System.out.println("Enter the second no. ");
+        int b=sc.nextInt();
+        
+
+        int c = a+b;
+
+        System.out.print9
+        9("The sum is: "+c);
+
+    }
+    
+}
