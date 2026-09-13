6f79f7c0dadf62af101780f483f16ec762119777
Kishan Dubey <ishandubey0001@gmail.com>
2026-09-13 23:45:33 +0530

    fgh

---
+++ day01/mydetail.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class mydetail {
+    public static void main(String[] args){
+        String name="Ayush";
+        int age = 90;
+        String uni = "BBD";
+        String course ="BTech";
+
+
+        System.out.println("name is: "+name);
+    }
+}
